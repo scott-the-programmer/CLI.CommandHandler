7046437e87a2b56fef4de2f7aa5c545a39904484
Scott Murray <scottam.coder@gmail.com>
2021-07-04 14:28:20 +1200

    Remove unused ctors in CommandDispatchException

--- CLI.CommandHandler/CommandDispatchException.cs
+++ CLI.CommandHandler/CommandDispatchException.cs
@@ -6,32 +6,13 @@ namespace CLI.CommandHandler
     [Serializable]
     public class CommandDispatchException : Exception
     {
-        public CommandDispatchException()
-        {
-        }
-
         public CommandDispatchException(string message)
             : base(message)
         {
         }
 
-        public CommandDispatchException(string format, params object[] args)
-            : base(string.Format(format, args))
-        {
-        }
-
         public CommandDispatchException(string message, Exception innerException)
             : base(message, innerException)
-        {
-        }
-
-        public CommandDispatchException(string format, Exception innerException, params object[] args)
-            : base(string.Format(format, args), innerException)
-        {
-        }
-
-        protected CommandDispatchException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
         {
         }
     }
